41c7c69b1acee06e55a186f6ca28d8ebc780a523
gabriell122 <gabriell.haines@etec.sp.gov.br>
2024-11-06 16:30:43 -0300

    Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia( "12345", "tijolo", "12345", 1024);
+nokia.InstalarAplicativo("minecraft");
+nokia.Ligar();
+nokia.ReceberLigacao();
+Console.WriteLine($"O número do seus celular é {nokia.Numero}\nO Imae é {nokia.Imei} \nO modelo é {nokia.Modelo} \nE a memoria e de {nokia.Memoria}");
+
+Console.WriteLine();
+Iphone iphone = new Iphone( "54321", "6tadoido", "54321", 512);
+iphone.InstalarAplicativo("pou");
+iphone.Ligar();
+iphone.ReceberLigacao();
+Console.WriteLine($"O número do seus celular é {iphone.Numero}\nO Imae é {iphone.Imei} \nO modelo é {iphone.Modelo} \nE a memoria e de {iphone.Memoria}");
+
+
+
